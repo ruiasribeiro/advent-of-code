d25af75e05324407c156cea3aa2403e7318ca33a
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2020-12-04 22:33:40 +0000

    Add day 1

---
+++ day1/day1.py
@@ -0,0 +1,48 @@
+# day 1 of advent of code
+
+
+
+## part 1 
+
+# read input file
+with open('day1/input', 'r') as fp:
+    line = fp.readline()
+    nums = [int(line)]
+
+    while line:
+        line = fp.readline()
+        if line != '': 
+            nums.append(int(line))
+
+# determine the two entries
+res = None
+for n1 in nums:
+    for n2 in nums:
+        if n1 != n2 and n1 + n2 == 2020:
+            res = n1 * n2
+            break
+    if res != None:
+        break
+
+# print the result
+print(res)
+
+
+
+## part 2
+
+# determine the three entries
+res = None
+for n1 in nums:
+    for n2 in nums:
+        for n3 in nums:
+            if n1 != n2 and n2 != n3 and n1 + n2 + n3 == 2020:
+                res = n1 * n2 * n3
+                break
+        if res != None:
+            break
+    if res != None:
+        break
+
+# print the result
+print(res)
