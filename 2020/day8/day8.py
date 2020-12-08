70fd36fc8a4f777f6ae066c9d7e8cfee1e246d3d
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2020-12-08 22:29:49 +0000

    Add part 2 of day 8

--- 2020/day8/day8.py
+++ 2020/day8/day8.py
@@ -34,6 +34,45 @@ def main():
 
     print(acc)
 
+    # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    ## part 2
+
+    n_instr = len(instructions)
+    instr_original = instructions.copy()
+    acc = 0
+
+    # change instruction one by one and test each time
+    for idx in range(0, n_instr):
+        instructions = instr_original.copy()
+        change = instructions[idx]
+
+        if change[0] == 'nop' and change[1] != 0:
+            instructions[idx] = ('jmp', change[1])
+        elif change[0] == 'jmp':
+            instructions[idx] = ('nop', change[1])
+
+        executed = [False] * n_instr
+        acc = 0
+        i = 0
+        while i < n_instr:
+            if executed[i]:
+                break
+            executed[i] = True
+            instr = instructions[i]
+            if instr[0] == 'acc':
+                acc += instr[1]
+                i += 1
+            elif instr[0] == 'jmp':
+                i += instr[1]
+            elif instr[0] == 'nop':
+                i += 1
+
+        if i == n_instr:
+            break
+        
+    print(acc)
+    
 
 if __name__ == '__main__':
     main()
