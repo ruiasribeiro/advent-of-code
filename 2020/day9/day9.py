956fa2e3f3820e51990bbb1c5bd628d46511ccd1
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2020-12-09 22:54:29 +0000

    Add part 2 of day 9

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -1,9 +1,10 @@
 # day 9 of advent of code
 
 
-def main():
+preamble = 25
 
-    preamble = 25
+
+def part1():
     
     with open('day9/input', 'r') as fp:
         line = fp.readline()
@@ -36,6 +37,54 @@ def main():
     
     print(res)
 
+
+
+def part2():
+
+    result = 400480901
+    
+    with open('day9/input', 'r') as fp:
+        line = fp.readline()
+        nums = []
+        reached_max = False
+    
+        while line and not reached_max:
+            nums.append(int(line))
+            if len(nums) == preamble:
+                reached_max = True
+            line = fp.readline()
+
+        fst = 0
+        lst = 0
+        found = False
+        
+        while line:
+            fst = 0
+            lst = 0
+            while not found and lst <= preamble:
+                total = sum(nums[fst:lst])
+                if total == result:
+                    found = True
+                elif total > result:
+                    fst += 1
+                else:
+                    lst += 1
+
+            if found:
+                break
+
+            nums.append(int(line))
+            nums.pop(0)
+            line = fp.readline()
+    
+    list = nums[fst:lst]
+    print(min(list) + max(list))
+
+
+
+def main():
+    part1()
+    part2()
 
 
 if __name__ == '__main__':
