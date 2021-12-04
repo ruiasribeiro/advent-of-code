b643b9e87894142bc87562edfe3de83229160757
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2021-12-04 14:05:17 +0000

    [2021/03] Add part 2 of Haskell solution

--- 2021/day03/main.hs
+++ 2021/day03/main.hs
@@ -14,6 +14,11 @@ count (x : xs) =
         '0' -> (zeroes + 1, ones)
         '1' -> (zeroes, ones + 1)
 
+toDec :: String -> Int
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+-- part 1
+
 gamma :: [(Int, Int)] -> String
 gamma [] = ""
 gamma ((z, o) : t)
@@ -27,8 +32,23 @@ invert (x : xs) =
     '0' -> '1' : invert xs
     '1' -> '0' : invert xs
 
-toDec :: String -> Int
-toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+-- part 2
+
+oxygen :: Int -> [String] -> String
+oxygen _ [x] = x
+oxygen col l
+  | z > o = oxygen (col + 1) $ filter (('0' ==) . (!! col)) l
+  | z <= o = oxygen (col + 1) $ filter (('1' ==) . (!! col)) l
+  where
+    (z, o) = count $ (!! col) $ transpose l
+
+dioxide :: Int -> [String] -> String
+dioxide _ [x] = x
+dioxide col l
+  | z > o = dioxide (col + 1) $ filter (('1' ==) . (!! col)) l
+  | z <= o = dioxide (col + 1) $ filter (('0' ==) . (!! col)) l
+  where
+    (z, o) = count $ (!! col) $ transpose l
 
 -- main
 
@@ -38,3 +58,6 @@ main = do
   let gm = gamma $ map count $ transpose $ lines contents
   let ep = invert gm
   printf "Part 1: %d\n" (toDec gm * toDec ep)
+  let ox = oxygen 0 $ lines contents
+  let di = dioxide 0 $ lines contents
+  printf "Part 2: %d\n" (toDec ox * toDec di)
